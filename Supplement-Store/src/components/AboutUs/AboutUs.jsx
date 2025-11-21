b78e0fee73f04306e35328c86c855685a8b0ea12
Fathy Mohamed <fathymohameed22@gmail.com>
2025-11-21 17:38:35 +0200

    C

--- Supplement-Store/src/components/AboutUs/AboutUs.jsx
+++ Supplement-Store/src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Login() {
-    return (
-        <div>
-        
-        </div>
-    )
+export default function AboutUs() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
--- Supplement-Store/src/components/Coaches/Coaches.jsx
+++ Supplement-Store/src/components/Coaches/Coaches.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function Login() {
-    return (
-        <div>
-        
-        </div>
-    )
+export default function Coaches() {
+  return (
+    <div>
+      
+    </div>
+  )
 }
