06c6aff7eee471c7863e9b1c7d9ded7078a1a018
Ziad Elhefnawy <ziad.amr.elhefnawy@gmail.com>
2025-12-06 02:45:17 +0200

    added Scroll bar style

--- Supplement-Store/src/components/Layout/Layout.jsx
+++ Supplement-Store/src/components/Layout/Layout.jsx
@@ -3,13 +3,12 @@ import Navbar from '../Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Footer/Footer'
 
-
 export default function Layout() {
     return (
         <>
-        <Navbar />
-        <Outlet></Outlet>
-        <Footer/>
+            <Navbar />
+            <Outlet />
+            <Footer />
         </>
     )
 }
